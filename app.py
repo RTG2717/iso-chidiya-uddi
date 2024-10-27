b5f7bc2ded3450073bde498a085b49e8c8760bba
FlyingSaturn <56539009+FlyingSaturn@users.noreply.github.com>
2024-10-28 01:56:20 +0530

    Keep the below box visible during the game

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ clock = pygame.time.Clock()
 bg = pygame.Surface((w, h))
 bg.fill((251,176,59))
 prompt_box = pygame.Surface((w/4,h/10))
-answer_box = pygame.Surface((w/4,h/10))
+# answer_box = pygame.Surface((w/4,h/10))
 # table = pygame.Surface((w/
 
 while True:
@@ -23,7 +23,7 @@ while True:
     
     screen.blit(bg, (0, 0))
     # screen.blit(table, (300, ))
-    screen.blit(prompt_box, (w/8*3, h/16*2))
-    screen.blit(answer_box, (w/8*3, h - h/16*2 - h/10))
+    # screen.blit(answer_box, (w/8*3, h/16*2))
+    screen.blit(prompt_box, (w/8*3, h - h/16*2 - h/10))
     pygame.display.update()
     clock.tick(60)
