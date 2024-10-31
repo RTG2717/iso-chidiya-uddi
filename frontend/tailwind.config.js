4e361da9833be666f09bf87caf4e2acd5139ce6c
Digit4 <dhaval.pantojee@gmail.com>
2024-10-31 19:15:29 +0530

    Tailwind misconfiguration fix + instructions on how to update.

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-    content: ["./src/**/*.{js,ts,jsx,tsx}"],
+export default {
+    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
     theme: {
-      extend: {},
+        extend: {},
     },
     plugins: [],
-  }
+};
