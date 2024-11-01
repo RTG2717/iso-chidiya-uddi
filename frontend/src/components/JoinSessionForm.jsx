4269bb3f292c3edd527f17c91e3437d4e1c9c9b0
Digit4 <dhaval.pantojee@gmail.com>
2024-11-02 00:33:04 +0530

    join session button functionality added (user pastes link in the inut box and presses join, it makes the user join the game)

--- frontend/src/components/JoinSessionForm.jsx
+++ frontend/src/components/JoinSessionForm.jsx
@@ -1,18 +1,61 @@
+import { useState } from 'react';
 import Input from './Input';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 const JoinSessionForm = ({ isJoining }) => {
+    const [sessionLink, setSessionLink] = useState('second');
     const inputTextCSS =
         'px-2 py-1 w-96 rounded-md bg-white placeholder bg-indigo-50 mx-2';
 
     const buttonCSS = 'px-2 py-1 rounded-md bg-indigo-600 text-stone-200';
+
+    const navigate = useNavigate();
+
+    const handleLinkInput = (e) => {
+        setSessionLink(e.target.value);
+    };
+
+    const verifyLink = (e) => {
+        e.preventDefault();
+        const sessionID = new URLSearchParams(sessionLink?.split('?')[1])?.get(
+            'session'
+        );
+        axios
+            .get(
+                [import.meta.env.VITE_API_URL, 'api/sessions', sessionID].join(
+                    '/'
+                )
+            )
+            .then((res) => {
+                // if response is 200 then redirect to the url
+                if (res.status === 200) {
+                    console.log('link', res.data.sessionID);
+                    navigate(`/track?session=${res.data.sessionID}`);
+                }
+            })
+            .catch((error) => {
+                console.error(
+                    'Error occurred trying to fetch sessionID: ',
+                    error
+                );
+            });
+        console.log('sessionLink', sessionID);
+    };
     return (
         <form hidden={!isJoining}>
             <Input
                 placeholder='Enter the URL sent by your friend'
                 className={'placeholder:text-slate-500 ' + inputTextCSS}
                 type='text'
+                onChange={handleLinkInput}
             />
-            <Input className={buttonCSS} type='submit' value='Enter Lobby' />
+            <Input
+                onClick={verifyLink}
+                className={buttonCSS}
+                type='submit'
+                value='Enter Lobby'
+            />
         </form>
     );
 };
