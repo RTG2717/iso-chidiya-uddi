cd31287b0a8095329eae7e6a32b88614bfbd1056
Digit4 <dhaval.pantojee@gmail.com>
2024-11-01 19:14:26 +0530

    rewritten App.jsx to use routes + Routes added to Layout.jsx

---
+++ frontend/src/components/Layout.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import CursorTracker from './cursorTracker';
+import UsernameForm from './UsernameForm';
+
+const Layout = () => {
+    return (
+        <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/track' element={<CursorTracker />} />
+            <Route path='/create-new-lobby' element={<UsernameForm />} />
+        </Routes>
+    );
+};
+
+export default Layout;
