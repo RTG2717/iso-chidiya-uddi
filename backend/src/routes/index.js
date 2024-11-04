1f7ab0cccc805e9bc684dd31f438681cee11880a
Digit4 <dhaval.pantojee@gmail.com>
2024-11-04 12:42:31 +0530

    added client route to index

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const sessionRoutes = require('./api/session.routes');
+const clientRoutes = require('./api/client.routes');
 
 router.use('/api/sessions', sessionRoutes);
+router.use('/api/clients', clientRoutes);
 
 module.exports = router;
