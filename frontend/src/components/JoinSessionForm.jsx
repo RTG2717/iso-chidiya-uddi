46eabbd9e45210732197b38b07c870ef3f85e880
Digit4 <dhaval.pantojee@gmail.com>
2024-11-12 18:25:53 +0530

    changed sessionLink handler to sessionCode handler and using sessionCode from store.

--- frontend/src/components/JoinSessionForm.jsx
+++ frontend/src/components/JoinSessionForm.jsx
@@ -1,12 +1,11 @@
 import { useState } from 'react';
+import useStore from '../store';
 import Input from './Input';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
-const baseURL = import.meta.env.VITE_API_URL;
-
 const JoinSessionForm = ({ isJoining }) => {
-    const [sessionLink, setSessionLink] = useState('second');
+    const { apiURL, sessionCode, setSessionCode } = useStore();
     const inputTextCSS =
         'px-2 py-1 w-24 rounded-md bg-white placeholder bg-indigo-50 mx-2';
 
@@ -14,19 +13,18 @@ const JoinSessionForm = ({ isJoining }) => {
 
     const navigate = useNavigate();
 
-    const handleLinkInput = (e) => {
-        setSessionLink(e.target.value);
+    const handleCodeInput = (e) => {
+        setSessionCode(e.target.value);
     };
 
     const verifyCode = (e) => {
         e.preventDefault();
 
-        const sessionCode = e.target[0].value;
         axios
-            .get(`${baseURL}/api/sessions_by_code/${sessionCode}`)
+            .get(`${apiURL}/api/sessions/code/${sessionCode}`)
             .then((res) => {
                 if (res.status === 200) {
-                    navigate(`/join-lobby`, { state: { session: res.data } });
+                    navigate(`/join-lobby`);
                 }
             })
             .catch((err) => {
@@ -69,7 +67,7 @@ const JoinSessionForm = ({ isJoining }) => {
                 placeholder='Enter Code'
                 className={'placeholder:text-slate-500 ' + inputTextCSS}
                 type='text'
-                onChange={handleLinkInput}
+                onChange={handleCodeInput}
             />
             <Input
                 // onClick={verifyLink}
